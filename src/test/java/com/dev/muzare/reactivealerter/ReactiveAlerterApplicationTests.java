e6a79e1dd51af978b580336658aec89a14d5866c
Murtaza <muzareali@gmail.com>
2024-03-17 14:53:34 +0530

    Initial Commit

---
+++ src/test/java/com/dev/muzare/reactivealerter/ReactiveAlerterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.muzare.reactivealerter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveAlerterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
